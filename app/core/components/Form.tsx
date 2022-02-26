a55c5676ee888e40e4b0df65cbbbe0c78c7e3e00
파링 <parangee9706@gmail.com>
2022-02-26 19:00:00 +0900

    something

--- app/core/components/Form.tsx
+++ app/core/components/Form.tsx
@@ -1,5 +1,5 @@
 import { ReactNode, PropsWithoutRef } from "react"
-import { Form as FinalForm, FormProps as FinalFormProps } from "react-final-form"
+import { Form as FinalForm, FormProps as FinalFormProps, FormRenderProps } from "react-final-form"
 import { z } from "zod"
 import { validateZodSchema } from "blitz"
 export { FORM_ERROR } from "final-form"
@@ -7,7 +7,7 @@ export { FORM_ERROR } from "final-form"
 export interface FormProps<S extends z.ZodType<any, any>>
   extends Omit<PropsWithoutRef<JSX.IntrinsicElements["form"]>, "onSubmit"> {
   /** All your form fields */
-  children?: ReactNode
+  children: (props: FormRenderProps) => ReactNode
   /** Text to display in the submit button */
   submitText?: string
   schema?: S
@@ -28,30 +28,27 @@ export function Form<S extends z.ZodType<any, any>>({
       initialValues={initialValues}
       validate={validateZodSchema(schema)}
       onSubmit={onSubmit}
-      render={({ handleSubmit, submitting, submitError }) => (
-        <form onSubmit={handleSubmit} className="form" {...props}>
-          {/* Form fields supplied as children are rendered here */}
-          {children}
+      render={(formProps) => {
+        const { handleSubmit, submitting, submitError } = formProps
+        return (
+          <form onSubmit={handleSubmit} className="form" {...props}>
+            {/* Form fields supplied as children are rendered here */}
+            {children(formProps)}
 
-          {submitError && (
-            <div role="alert" style={{ color: "red" }}>
-              {submitError}
-            </div>
-          )}
+            {submitError && (
+              <div role="alert" style={{ color: "red" }}>
+                {submitError}
+              </div>
+            )}
 
-          {submitText && (
-            <button type="submit" disabled={submitting}>
-              {submitText}
-            </button>
-          )}
-
-          <style global jsx>{`
-            .form > * + * {
-              margin-top: 1rem;
-            }
-          `}</style>
-        </form>
-      )}
+            {submitText && (
+              <button type="submit" disabled={submitting}>
+                {submitText}
+              </button>
+            )}
+          </form>
+        )
+      }}
     />
   )
 }
--- app/game/layout/index.tsx
+++ app/game/layout/index.tsx
@@ -3,8 +3,7 @@ import Sidebar from "./Sidebar"
 import { useCurrentUser } from "../../core/hooks/useCurrentUser"
 import { RevolvingDot } from "react-loader-spinner"
 import LoginButton from "../../core/components/LoginButton"
-import { useRecoilState } from "recoil"
-import { SOverlayContent } from "../utils/store"
+import { useScale } from "../utils/scale"
 
 const GameLayout: React.FC = (props) => {
   return (
@@ -31,24 +30,11 @@ const GameLayout: React.FC = (props) => {
 
 const GameLayoutContent: React.FC = ({ children }) => {
   const user = useCurrentUser()
-  const containerRef = React.useRef<HTMLElement | null>()
-  const [scale, setScale] = React.useState(0)
-  const [overlay, setOverlay] = useRecoilState(SOverlayContent)
-
-  React.useEffect(() => {
-    const l = () => {
-      setScale(Math.min(document.body.clientWidth / 1920, document.body.clientHeight / 1080))
-    }
-    window.addEventListener("resize", l)
-    setScale(Math.min(document.body.clientWidth / 1920, document.body.clientHeight / 1080))
-    return () => {
-      window.removeEventListener("resize", l)
-    }
-  }, [])
+  const scale = useScale()
 
   return (
     <>
-      <div className="container" ref={(i) => (containerRef.current = i)}>
+      <div className="container gameLayout">
         {user ? (
           <div
             className="content"
@@ -57,7 +43,9 @@ const GameLayoutContent: React.FC = ({ children }) => {
             }}
           >
             <Sidebar />
-            <div className="card">{children}</div>
+            <div className="card" style={{ padding: 60 }}>
+              {children}
+            </div>
           </div>
         ) : (
           <div className="login">
@@ -69,8 +57,6 @@ const GameLayoutContent: React.FC = ({ children }) => {
             background: linear-gradient(135deg, #00c98d 0%, #72c8ae 100%);
             width: 100vw;
             height: 100vh;
-            overflow: hidden;
-            position: relative;
           }
           .card {
             background: rgba(255, 255, 255, 0.2);
@@ -88,7 +74,7 @@ const GameLayoutContent: React.FC = ({ children }) => {
           .content {
             width: 1920px;
             height: 1080px;
-            position: absolute;
+            position: fixed;
             padding: 60px;
             top: 50%;
             left: 50%;
@@ -98,45 +84,26 @@ const GameLayoutContent: React.FC = ({ children }) => {
           }
         `}</style>
       </div>
-      {overlay && (
-        <>
-          <div
-            style={{
-              position: "fixed",
-              zIndex: 9998,
-              background: "rgba(0, 0, 0, 0.5)",
-              width: "100%",
-              height: "100%",
-              left: 0,
-              top: 0,
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-            onClick={() => setOverlay(null)}
-          />
-          <div
-            onClick={(e) => {
-              e.preventDefault()
-            }}
-            style={{
-              background: "#6BD0B8",
-              borderRadius: 20,
-              padding: 40,
-              display: "flex",
-              flexDirection: "column",
-              gap: 20,
-              zIndex: 9999,
-              position: "fixed",
-              left: "50%",
-              top: "50%",
-              transform: `translate(-50%, -50%) scale(${scale})`,
-            }}
-          >
-            {overlay}
-          </div>
-        </>
-      )}
+      {/*{overlay && (*/}
+      {/*  <>*/}
+      {/*    <div*/}
+      {/*      style={{*/}
+      {/*        position: "fixed",*/}
+      {/*        zIndex: 9998,*/}
+      {/*        background: "rgba(0, 0, 0, 0.5)",*/}
+      {/*        width: "100%",*/}
+      {/*        height: "100%",*/}
+      {/*        left: 0,*/}
+      {/*        top: 0,*/}
+      {/*        display: "flex",*/}
+      {/*        justifyContent: "center",*/}
+      {/*        alignItems: "center",*/}
+      {/*      }}*/}
+      {/*      onClick={() => setOverlay(null)}*/}
+      {/*    />*/}
+
+      {/*  </>*/}
+      {/*)}*/}
     </>
   )
 }
