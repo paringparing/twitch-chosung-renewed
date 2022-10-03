c71776a264e7d8e3eff080e8a60f0d674dbb34ff
pikokr <parangee9706@gmail.com>
2022-10-03 10:26:50 +0900

    chore: character limit

--- app/game/validation/createCustomCategory.ts
+++ app/game/validation/createCustomCategory.ts
@@ -1,7 +1,7 @@
 import { z } from "zod"
 
 export default z.object({
-  name: z.string().min(1),
-  description: z.string().min(1),
+  name: z.string().min(1).max(32),
+  description: z.string().min(1).max(256),
   difficulty: z.number().int().min(1).max(10),
 })
--- app/game/validation/editCustomCategory.ts
+++ app/game/validation/editCustomCategory.ts
@@ -2,8 +2,8 @@ import { z } from "zod"
 
 export default z.object({
   id: z.number(),
-  name: z.string().min(1),
-  description: z.string().min(1),
+  name: z.string().min(1).max(32),
+  description: z.string().min(1).max(256),
   difficulty: z.number().int().min(1).max(10),
   visibility: z.literal("PUBLIC").or(z.literal("PRIVATE")),
   words: z.array(
