9e6e4785885908925e0320c5edb4e746a71fc977
pikokr <parangee9706@gmail.com>
2022-10-03 10:25:43 +0900

    feat: rule create/delete webhook

--- app/game/mutations/categories/createCustomCategory.ts
+++ app/game/mutations/categories/createCustomCategory.ts
@@ -1,11 +1,21 @@
 import { resolver } from "@blitzjs/rpc"
 import schema from "../../validation/createCustomCategory"
 import db from "../../../../db"
+import { sendWebhook } from "integrations/discord"
+import { Colors, EmbedBuilder } from "discord.js"
 
 export default resolver.pipe(
   resolver.authorize(["USER", "ADMIN"]),
   resolver.zod(schema),
   async (i, { session }) => {
+    const currentUser = await db.user.findFirst({
+      where: {
+        id: session.userId,
+      },
+    })
+
+    if (!currentUser) return "User not found"
+
     const item = await db.customCategory.create({
       data: {
         name: i.name,
@@ -14,6 +24,34 @@ export default resolver.pipe(
         ownerId: session.userId,
       },
     })
+
+    await sendWebhook(
+      {
+        embeds: [
+          new EmbedBuilder()
+            .setColor(Colors.Blue)
+            .setTitle("주제 추가됨")
+            .addFields(
+              {
+                name: "ID",
+                value: `${item.id}`,
+                inline: true,
+              },
+              {
+                name: "이름",
+                value: item.name,
+                inline: true,
+              },
+              {
+                name: "난이도",
+                value: `${item.difficulty}/10`,
+                inline: true,
+              }
+            ),
+        ],
+      },
+      currentUser
+    )
 
     return item.id
   }
--- app/game/mutations/categories/deleteCustomCategory.ts
+++ app/game/mutations/categories/deleteCustomCategory.ts
@@ -1,9 +1,19 @@
 import { resolver } from "@blitzjs/rpc"
 import db from "db"
+import { Colors, EmbedBuilder } from "discord.js"
+import { sendWebhook } from "integrations/discord"
 
 export default resolver.pipe(
   resolver.authorize(["USER", "ADMIN"]),
   async (i: number, { session }) => {
+    const currentUser = await db.user.findFirst({
+      where: {
+        id: session.userId,
+      },
+    })
+
+    if (!currentUser) return "User not found"
+
     const item = await db.customCategory.findFirst({
       where: {
         id: i,
@@ -16,6 +26,35 @@ export default resolver.pipe(
         id: item.id,
       },
     })
+
+    await sendWebhook(
+      {
+        embeds: [
+          new EmbedBuilder()
+            .setColor(Colors.Blue)
+            .setTitle("주제 삭제됨")
+            .addFields(
+              {
+                name: "ID",
+                value: `${item.id}`,
+                inline: true,
+              },
+              {
+                name: "이름",
+                value: item.name,
+                inline: true,
+              },
+              {
+                name: "난이도",
+                value: `${item.difficulty}/10`,
+                inline: true,
+              }
+            ),
+        ],
+      },
+      currentUser
+    )
+
     return null
   }
 )
